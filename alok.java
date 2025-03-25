3dbb79577f791b4cc671634cba721fa9d26741a4
Sndubey <sndubey7718@gmail.com>
2025-03-25 15:49:36 +0530

    some changes

--- alok.java
+++ alok.java
@@ -4,6 +4,8 @@ public class alok {
     public static void main(String[] args)
     {
         System.out.println("Name:Alok singh, Rollnumber: 187");
+        System.out.println("this is my first practical");
+        
     }
 }
 }
